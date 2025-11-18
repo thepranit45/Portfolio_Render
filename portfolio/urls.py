bd5be2e90e445324d93a4eb2c5983e0e8e94d770
Pranit Gopale <124279613+thepranit45@users.noreply.github.com>
2025-11-18 10:46:22 +0530

    done

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('skills/', views.skills, name='skills'),
     path('projects/', views.projects, name='projects'),
-    path('projects/<slug:slug>/', views.project_detail, name='project_detail'),
     path('projects/student-result/', views.project_student_result, name='project_student_result'),
+    path('projects/<slug:slug>/', views.project_detail, name='project_detail'),
     path('contact/', views.contact, name='contact'),
 ]
